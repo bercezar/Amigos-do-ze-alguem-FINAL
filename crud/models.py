eff022326c65474d35c5b99c4de9b23e4e2cc904
Ruan Correia <correia.ruan@gmail.com>
2024-11-10 17:51:46 -0300

    first commit

---
+++ crud/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    apelido = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Beneficiario(models.Model):
+    nome = models.CharField(max_length=100)
+    apelido = models.CharField(max_length=50)
+    documento = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    data_registro = models.DateField(default=timezone.now)
+    observacoes = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Voluntario(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.nome
