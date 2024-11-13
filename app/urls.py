07604879680cdf0e6cea183de134a12786622758
Ruan Correia <correia.ruan@gmail.com>
2024-11-13 11:09:49 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crud/', include('crud.urls'))
+    path('crud/', include('crud.urls')),
+    path('', views.home, name='home')
 ]
 
 if settings.DEBUG:
